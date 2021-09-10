921f9a44c97fbc1ba81c174dedd69d0c1e19d030
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2021-09-10 15:42:18 +0900

    Initial Commit

---
+++ StarSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarSample
+//
+//  Created by 三浦　一真 on 2021/09/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
