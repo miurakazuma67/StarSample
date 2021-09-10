1ddce4d648e0948b1b3f16c8b3479e89fdbd30dc
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2021-09-10 23:57:52 +0900

    first

--- StarSample/ViewController.swift
+++ StarSample/ViewController.swift
@@ -7,11 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var starView2: StarView!
+    @IBOutlet weak var starView2: StarView!
+    
+    enum StarView: String {
+        case star1 = starView1
+        case star2 = starView2
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
